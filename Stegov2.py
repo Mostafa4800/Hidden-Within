0a1e2e7f16e6850f52596e6644c1fe354271f2d6
Mostafa4800 <mostafaabbashassan@gmail.com>
2025-11-18 12:57:25 +0100

    Update steganography script with interactive encoding/decoding and remove unused image files

--- Stegov2.py
+++ Stegov2.py
@@ -71,11 +71,34 @@ def decode_image(image_location):
 
                 
                 
-
-
+def main():
+    print("Steganography using GCD Pattern")
+    while True:
+        choice = input("Choose (e)ncode, (d)ecode or (q)uit: ").lower()
+        if choice == 'e':
+            image_location = input("Enter image file path: ")
+            msg = input("Enter message to hide: ")
+            encoded_img = encode_image(image_location, msg)
+            output_path = input("Enter output image file path: ")
+            encoded_img.save(output_path)
+            print(f"Message encoded and saved to {output_path}")
+            continue
+        elif choice == 'd':
+            image_location = input("Enter image file path: ")
+            hidden_msg = decode_image(image_location)
+            print("Hidden message:", hidden_msg)
+            continue
+        elif choice == 'q':
+            print("Quitting the program.")
+            break
+        else:
+            print("Invalid choice. Please choose 'e', 'd', or 'q'.")
+            continue
 
 if __name__ == '__main__':
-    out = encode_image("test.png", "fuck you!!!")
-    out.save("encoded.png")
-    decoded_message = decode_image("encoded.png")
-    print(decoded_message)
+    main()
+    
+    #out = encode_image("test.jpg", "fuck you!!!")
+    #out.save("encoded.png")
+    #decoded_message = decode_image("encoded.png")
+    #print(decoded_message)
