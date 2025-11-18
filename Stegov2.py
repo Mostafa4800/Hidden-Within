b8e88e35534eec2e00a4f730a9a701514bf8c4fa
Mostafa4800 <mostafaabbashassan@gmail.com>
2025-11-18 10:15:02 +0100

    Refactor image handling and encoding logic in steganography script

--- Stegov2.py
+++ Stegov2.py
@@ -1,5 +1,5 @@
 from PIL import Image
-import hashes
+
 
 #convert text to binary to use later for embedding in the image
 
@@ -17,20 +17,51 @@ def binary_to_text(binary_str):
 def gcd(x,y):
     while y != 0:
         x, y=y, y % x
-    print(y, x)
+    return x
 
 #get the image the user wanna use to embed data to 
 
-def get_image(loc):
-    img = Image.open(loc, 'r')
-    pix_val = list(img.getdata())
-    print(pix_val)
+def get_image(location):
+    img = Image.open(location).convert("RGB")
+    pixels = img.load()
+    w, h = img.size
+    return img, pixels, w, h
 
-# Add logic to embed data in image:
+def encode_image(image_location, msg):
+    binary = text_to_binary(msg)   # conveert message to binary
+    bit_index = 0 # index to keep track
+
+    img, pixels, w, h = get_image(image_location) # get image and its pixels
+    pattern = gcd(w, h) # calculate the pattern using gcd
+
+    for i in range(h): # iterate through height
+        for j in range(w): # iterate through width
+
+            # logic
+            if ((i + 1) * (j + 1)) % pattern == 0: # position matches the pattern
+
+                # check if we're out of bits
+                if bit_index >= len(binary):
+                    # end marker → red = 0
+                    r, g, b = pixels[j, i]
+                    pixels[j, i] = (0, g, b)
+                    return img
+
+                # read next bit
+                bit = int(binary[bit_index])
+                bit_index += 1
+
+                # modify LSB so the color red
+                r, g, b = pixels[j, i]
+                r = (r & ~1) | bit
+                pixels[j, i] = (r, g, b)
+
+    return img  # return the image
 
 
 
 if __name__ == '__main__':
-    gcd(600, 400)
-    get_image("test2.png")
+    out = encode_image("test.png", "Hello World")
+    out.save("encoded.png")
+
     pass #main()
